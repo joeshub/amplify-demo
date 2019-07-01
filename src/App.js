f63b85cf77c3d5c497a89450a2f0a2e134913bad
Joe Seifi <764006+joeshub@users.noreply.github.com>
2019-07-01 16:58:03 -0700

    base amplify sign up / sign in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
+
 import './App.css';
 
 function App() {
+  const [signUpStep, setSignUpStep] = useState(0);
+  const [username, setuserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone_number, setPhoneNumber] = useState('');
+  const [authenticationCode, setAuthenticationCode] = useState('');
+
+  const signUp = async () => {
+    try {
+      const resp = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+          phone_number,
+        }
+      });
+      console.log('Sign up successful', resp);
+      setSignUpStep(1);
+    } catch (ex) {
+      console.log('Sign up failed', ex);
+    }
+  }
+
+  const confirmSignUp = async () => {
+    try {
+      const resp = await Auth.confirmSignUp(username, authenticationCode);
+      console.log('Sign up successful', resp);
+    } catch (ex) {
+      console.log('Confirm sign up failed', ex);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {signUpStep === 0 && (
+        <>
+          <input type="text" name="username" placeholder="Username - email" onChange={e => setuserName(e.target.value)} />
+          <input type="password" name="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+          <input type="email" name="email" placeholder="Email Address" onChange={e => setEmail(e.target.value)} />
+          <input name="phone_number" placeholder="Phone Number" onChange={e => setPhoneNumber(e.target.value)} />
+          <button onClick={signUp}>Sign up</button>
+        </>
+      )}
+      {signUpStep === 1 && (
+        <>
+          <input type="text" name="username" placeholder="Username - email" onChange={e => setuserName(e.target.value)} />
+          <input type="text" name="authenticationCode" placeholder="Authentication Code" onChange={e => setAuthenticationCode(e.target.value)} />
+          <button onClick={confirmSignUp}>Confirm sign up</button>
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
+
